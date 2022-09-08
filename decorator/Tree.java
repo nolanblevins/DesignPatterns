6aac8d230e4e98606200abe1f2fefd1d667ec793
Nolan Blevins <npblevins@gmail.com>
2022-09-08 09:23:12 -0400

    created base classes

---
+++ decorator/Tree.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+public abstract class Tree {
+    protected ArrayList<String> lines;
+
+    public Tree(String lines) {
+        this.lines = new ArrayList<String>();
+    }
+    public String toString(String s)
+    {
+        return s;
+
+    }
+
+
+    
+}
---
+++ decorator/TreeDecorator.java
@@ -0,0 +1,6 @@
+import java.util.ArrayList;
+
+public abstract class TreeDecorator {
+    public ArrayList
+    
+}
