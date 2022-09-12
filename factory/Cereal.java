24569bdc658100dd8e0a10bd1814f70b3cad9f9c
Nolan Blevins <npblevins@gmail.com>
2022-09-12 13:17:30 -0400

    added base project files

---
+++ factory/Cereal.java
@@ -0,0 +1,13 @@
+package factory;
+
+import java.util.ArrayList;
+
+public abstract class Cereal {
+    protected String name;
+    protected double price;
+    protected ArrayList<String> toys;
+    
+    public Cereal(String name, double price){
+        
+    }
+}
---
+++ factory/GroceryStore.java
@@ -0,0 +1,10 @@
+package factory;
+
+public class GroceryStore {
+
+    public Cereal createCereal(String type){
+        return null;
+        
+    }
+    
+}
